613e9ce5bc821fdd4cc278b41e3de832ea141ddb
Alison Ludick <alison.ludick@gmail.com>
2019-09-03 19:18:13 +0200

    mouseover and mouseout to change image

--- js/index.js
+++ js/index.js
@@ -7,8 +7,36 @@ redHeader.addEventListener('click', (event) => {
     event.target.style.backgroundColor = 'red';
 })
 
-// mouseover event to change background color
+// mouseover event to change background color of Intro
 let changeIntro = document.querySelector('.intro');
 changeIntro.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'orange';
+})
+
+// dblclick event to change background color of text
+let textContent = document.querySelector('.text-content');
+textContent.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'silver';
+})
+
+// mouseover and mouseout to change image
+let imgDestination = document.querySelector('.dest-place');
+imgDestination.addEventListener('mouseover', () => {
+    imgDestination.src = 'img/destination2.jpg';
+})
+
+imgDestination.addEventListener('mouseout', () => {
+    imgDestination.src = 'img/destination1.jpg';
+})
+
+
+
+
+
+
+// change main navigation to block display??
+let mainNav = document.querySelector('.container .nav-container .nav .nav-links');
+
+mainNav.addEventListener('mouseover', () => {
+    mainNav.style.display = 'block';
 })
