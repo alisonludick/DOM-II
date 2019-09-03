e248f364cbcaae41e375892a3765c61c24d9b5a6
Alison Ludick <alison.ludick@gmail.com>
2019-09-03 15:15:52 +0200

    mouseclick event listener to change header background color to red

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Example of how to add an EventListener:
+// element.addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
+
+// mouseclick event listener to change header background color to red
+let redHeader = document.querySelector('.nav-container');
+redHeader.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'red';
+})
