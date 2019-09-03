ca6f7d2eb2524b8857d1f16042318930e8a05416
Alison Ludick <alison.ludick@gmail.com>
2019-09-03 15:38:40 +0200

    mouseover event to change intro background color to orange

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 // Example of how to add an EventListener:
 // element.addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
 
-// mouseclick event listener to change header background color to red
+// click event listener to change header background color to red
 let redHeader = document.querySelector('.nav-container');
 redHeader.addEventListener('click', (event) => {
     event.target.style.backgroundColor = 'red';
+})
+
+// mouseover event to change background color
+let changeIntro = document.querySelector('.intro');
+changeIntro.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'orange';
 })
